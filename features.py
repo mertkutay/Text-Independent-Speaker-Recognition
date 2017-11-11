4b3ef1d9f44a049e9a08c62e90970729c81be111
mertckutay <mertckutay@gmail.com>
2017-11-12 01:00:40 +0300

    moving

--- features.py
+++ features.py
@@ -3,28 +3,28 @@ from scipy import fftpack
 from scipy.io import wavfile
 
 
-def mfcc(file, num_ceps):
+def mfcc(file):
     rate, sig = wavfile.read(file)
     sig = pre_emphasis(sig, 0.97)
     frames = frame_signal(sig, rate, 0.025, 0.01)
     psd_frames = power_spectrum(frames, 512)
-    energies = fbank_energies(psd_frames, rate)
-    feats = fftpack.dct(energies, norm='ortho')[:, :num_ceps]
+    energies = fbank_energies(psd_frames, rate, 20)
+    feats = dist_dct(energies)
     feats = lifter(feats, 22)
-    feats = normalize(feats)
+    feats = normalize(feats, 6)
     # tot_energies = (np.log(np.sum(psd_frames, axis=1)))
     # feats[:, 0] = tot_energies
     return feats
 
 
-def dmfcc(file, num_ceps):
-    m = mfcc(file, num_ceps)
+def dmfcc(file):
+    m = mfcc(file)
     dm = delta(m)
     return np.concatenate((m, dm), axis=1)
 
 
-def ddmfcc(file, num_ceps):
-    m = mfcc(file, num_ceps)
+def ddmfcc(file):
+    m = mfcc(file)
     dm = delta(m)
     ddm = delta(dm)
     return np.concatenate((m, dm, ddm), axis=1)
@@ -50,8 +50,8 @@ def power_spectrum(frames, nfft):
     return (np.absolute(fft_frames) ** 2) / nfft
 
 
-def mel_fbanks(rate):
-    mels = np.linspace(hz_mel(0), hz_mel(rate/2), 28)
+def mel_fbanks(rate, num_ceps):
+    mels = np.linspace(hz_mel(0), hz_mel(rate/2), num_ceps+4)
     bins = ((512+1)*mel_hz(mels)/rate).astype(int)
     fbanks = np.zeros((len(bins)-2, bins[-1]-bins[0]+1))
     for i in range(len(fbanks)):
@@ -60,32 +60,51 @@ def mel_fbanks(rate):
     return fbanks
 
 
-def fbank_energies(psd_frames, rate):
-    fbanks = mel_fbanks(rate)
+def fbank_energies(psd_frames, rate, num_ceps):
+    fbanks = mel_fbanks(rate, num_ceps)
     energies = np.dot(psd_frames, fbanks.T)
     energies = np.where(energies == 0, np.finfo(float).eps, energies)
     return np.log(energies)
+
+
+def dist_dct(energies):
+    p = int(np.size(energies, 1)/2)
+    feats1 = fftpack.dct(energies[:, 0:p], type=2, norm='ortho')
+    feats2 = fftpack.dct(energies[:, p:], type=2, norm='ortho')
+    feats = np.concatenate((feats1[:, 1:p], feats2[:, 1:p]), axis=1)
+    return feats
 
 
 def lifter(feats, coeff):
     return (1 + coeff/2*np.sin(np.pi*np.arange(feats.shape[1])/coeff)) * feats
 
 
-def normalize(feats):
-    return (feats - np.mean(feats, axis=0))/np.var(feats, axis=0)
+def normalize(feats, window):
+    for i in range(len(feats)):
+        if i <= window:
+            low_ind = 0
+            high_ind = i + window + 1
+        elif i + window + 1 <= len(feats):
+            low_ind = i - window
+            high_ind = len(feats)
+        else:
+            low_ind = i - window
+            high_ind = i + window + 1
+        feats[i] = (feats[i] - np.mean(feats[low_ind: high_ind], axis=0))/np.var(feats[low_ind: high_ind], axis=0)
+    return feats
 
 
 def delta(feats):
     delta_feats = np.empty_like(feats)
     padded_feats = np.pad(feats, ((2, 2), (0, 0)), mode='edge')
     for i in range(len(delta_feats)):
-        delta_feats[i] = np.dot(np.arange(-2, 3), padded_feats[i:i+5])
-    return normalize(delta_feats)
+        delta_feats[i] = np.dot(np.arange(-2, 3), padded_feats[i:i+5]) / 10
+    return delta_feats
 
 
 def hz_mel(f):
-    return 1125*np.log(1+f/700)
+    return 2595*np.log10(1+f/700)
 
 
 def mel_hz(m):
-    return 700*(np.exp(m/1125)-1)
+    return 700*(10**(m/2595)-1)
--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import numpy as np
 import os
+#from keras.models import Sequential
+#from keras.layers import Dense, Activation
 
-from network import Network
 import features
 
 
 def main():
-    extract_feats('2003_nist_sre')
+    np.savetxt('test.txt', features.ddmfcc('test.wav'), fmt='%.4f')
+    # extract_feats('2003_nist_sre')
+    # model = construct_model()
 
 
 def extract_feats(directory):
@@ -14,7 +17,23 @@ def extract_feats(directory):
         wav_files = [file for file in files if file.endswith('.wav')]
         for file in wav_files:
             name = file.split('.')[0]
-            np.save(os.path.join(root, name), features.ddmfcc(os.path.join(root, file), 12))
+            np.save(os.path.join(root, name), features.mfcc(os.path.join(root, file)))
+            print(os.path.join(root, name)+'.npy')
+
+
+def construct_model():
+    model = Sequential()
+    model.add(Dense(32, activation='relu', input_dim=100))
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(optimizer='adam',
+                  loss='binary_crossentropy',
+                  metrics=['accuracy'])
+    return model
+
+
+def train_model(model):
+    with open('/2003_nist_sre/train_data/train_2sp/m_train.lst') as f:
+        lines = f.readlines()
 
 
 main()
